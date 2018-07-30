d1498367807cc2d3d63c7c1a65269cc692a60c3b
Nzingani <isabelnzingani@outlook.com>
2018-07-30 21:19:52 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>P5ex1</title>
+  </head>
+  <body>
+    <p>
+  <?php
+ $months = array ('Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septmbre','Octobre','Novembre','Décembre');
+
+  echo $months[];
+  ?>
+  </p>
+</body>
